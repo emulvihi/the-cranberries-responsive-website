379c2571d8c72a82701984beb8570f49713b6f32
Ubuntu <ubuntu@ip-172-31-44-170.ec2.internal>
2019-06-14 09:19:14 +0000

    Added fadeInUp animation to captions on carousel; Added notation to index.html; Styled navbar and footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,21 @@
+// Script to enable smooth scrolling on nav bar links
+
 $('a[href^="#"]').not('.carousel-control-icons').on('click', function(event) {
 
-            var target = $(this.getAttribute('href'));
+    var target = $(this.getAttribute('href'));
 
-            if (target.length) {
-                event.preventDefault();
-                $('html, body').stop().animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-            }
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
 
-        });
+});
+
+$('#carouselHeader').on('slide',function(){
+  $('.carousel-caption').fadeOut(300);
+})
+$('#carouselHeader').on('slid',function(){
+  $('.carousel-caption').fadeIn(600);
+})
